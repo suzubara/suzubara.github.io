e425be5de12dd146c9e368dc68a67f2d2eacdc87
Suzanne Rozier <srozier@squareup.com>
2025-04-15 17:34:16 -0500

    wip

--- src/components/generators.tsx
+++ src/components/generators.tsx
@@ -46,7 +46,8 @@ let one = generator.next();
 alert(JSON.stringify(one)); // {value: 1, done: false}
 `
 
-  const asyncExample = `async function f() {
+  const asyncExample = `async function myFunction() {
+  await someOtherFunction();
   return 1;
 }
 `
@@ -57,12 +58,17 @@ alert(JSON.stringify(one)); // {value: 1, done: false}
         <section>
           <h1>Async user flows with generators</h1>
           <h3>Suzanne Rozier</h3>
-          <h6>Austin JS 3/17/2025</h6>
+          <h6>Austin JS 4/15/2025</h6>
 
           <aside className="notes">
             Hi I'm Suzanne, I work at Square. At AustinJS a few months back
             someone was talking about generators, and said something about
             wanting to know if anyone had a production use case for them.
+            <br /><br />
+            I kind of have one? I learned about them at a previous job, where 
+            we had to build a complex onboarding flow. We used Redux sagas which 
+            use generators under the hood. So they're useful for building user 
+            flows, but why?
           </aside>
         </section>
         <section>
@@ -74,12 +80,16 @@ alert(JSON.stringify(one)); // {value: 1, done: false}
           </pre>
 
           <aside className="notes">
-            First: what is a generator? this is a lightning talk, not a deep
+            Ok so what is a generator? this is a lightning talk, not a deep
             dive, so I'm keeping it simple. A generator is a special kind of
-            function, you know it's special because of the star. It's like a
-            state machine. After defining a generator function, you invoke it
+            function, you know it's special because of the star.
+            <br /><br />
+            It's like a state machine. After defining a generator function, you invoke it
             and then call next to step through. It will pause at each yield.
             When it returns, it's done.
+            <br /><br />
+            so it's a function that can stop and wait for something before continuing.
+            Kind of like something else we know:
           </aside>
         </section>
         <section>
@@ -90,7 +100,13 @@ alert(JSON.stringify(one)); // {value: 1, done: false}
             </code>
           </pre>
 
-          <aside className="notes">First lets talk about async/await.</aside>
+          <aside className="notes">
+            async/await! Async functions also fall in this category, it can stop and
+            wait for something before it continues. but it can only wait for another
+            async function. like making a network call or reading a file - things that are done by computers.
+            <br /><br />
+            this is useful but what if we want to wait for something that is done by a human?
+            </aside>
         </section>
       </div>
     </div>
